9a975538b9bf652e7b0dc069c366980f1006e0cd
AlexGonzales-xd <alexjonasgs107@gmail.com>
2026-04-30 15:58:27 -0500

    PHP

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,7 +1,5 @@
 <?php
-
-// Clase para conectarse con la base de datos utilizando PDO.
-class Database {
+class Database {// Clase para conectarse con la base de datos utilizando PDO.
 
     // Guardamos la conexion con el modificador de acceso STATIC para que sea accesible sin necesidad de instanciar la clase.
     // El ? significa que la variable puede ser de tipo PDO o null, lo que permite manejar la conexión de manera segura y eficiente.
@@ -12,7 +10,15 @@ class Database {
         // Si en caso no encuentra la conexion.
          if(self::$connection === null) {
              // Armamos la conecion con los datos de .ENV
-             $dns= "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME";charset=utf8mb4";
+             $dns= "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME.";charset=utf8mb4";
+             // Creamos la conexion de PDO
+             self::$connection = new PDO($dns, DB_USER, DB_PASS,[
+                // Si hay error, lanza una excepción en de fallar la conexión, lo que facilita la depuración y el manejo de errores.
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                //Los resultados de las consultas vienen como arrays asociativos, lo que facilita el acceso a los datos utilizando nombres de columnas en lugar de índices numéricos.
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+             ]);
+              
          }
          //Creamos un retorno de valores
          return self::$connection;     
